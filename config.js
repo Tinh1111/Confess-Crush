b9344c083421ece7fa4e16d5bb986c81d21f2113
Tinh1111 <93986290+Tinh1111@users.noreply.github.com>
2021-11-09 19:50:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "phương uyên",
     introTitle: 'phương uyên',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao mày cứ đi nhầm đường
-    Lạc vào tim tao lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introDesc: `tao xin lỗi do tháng qua tao bị sốt
+    nên hong có tâm trạng nhắn với mi`,
+    btnIntro: 'ờ biết rồi',
+    title: 'cho tao xin lỗi nha cũng đến tháng 11 rồi mày đồng ý tao đi',
+    desc: 'zui dẻ hong quạo <333 ',
+    btnYes: 'tao đồng ý <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao mày lại yêu tao <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'tại vì tao cũng thick mày lâu rồi nhưng ngại <33333333',
+    mess: 'tao biết mà 🥰. Yêu mày nhiều nhiều 😘😘',
+    messDesc: 'tao đùa thôi,nếu thick tao thật thì rep tao nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
